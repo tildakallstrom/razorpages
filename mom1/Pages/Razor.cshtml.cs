394c4336b43eef0178747cda1ad16a1fbac213f1
Tilda Källström <tildakallstrom@gmail.com>
2022-01-19 08:55:29 +0100

    first commit

---
+++ mom1/Pages/Razor.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mom1.Pages
+{
+    public class RazorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ mom1/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+document.getElementById("footer").innerHTML = "This text was changed with JS";
+
